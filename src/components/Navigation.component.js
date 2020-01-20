161062ca5ee2e36717f98b3d8dad18380b3bc44d
sowmya dinavahi <sowmya.dsl@gmail.com>
2020-01-19 22:43:22 -0800

    Fix lint errors in navigation component

--- src/components/Navigation.component.js
+++ src/components/Navigation.component.js
@@ -1,29 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function Navigation(props) {
   const { onArrowClick, direction, symbol } = props;
-  const rightPos = direction === 'next' ? '25px' : null;
-  const leftPos = direction === 'prev' ? '25px' : null;
+  const rightPos = direction === "next" ? "25px" : null;
+  const leftPos = direction === "prev" ? "25px" : null;
   const style = {
-    position: 'absolute',
-    top: '50%',
+    position: "absolute",
+    top: "50%",
     right: rightPos,
     left: leftPos,
-    zIndex: '999',
-    color: '#fff',
-    height: '50px',
-    width: '50px',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    background: 'black',
-    borderRadius: '50%',
-    cursor: 'pointer',
-    transition: 'transform ease-in .1s',
-  }
+    zIndex: "999",
+    color: "#fff",
+    height: "50px",
+    width: "50px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    background: "black",
+    borderRadius: "50%",
+    cursor: "pointer",
+    fontSize: "1.5em",
+    transition: "transform ease-in .1s"
+  };
   return (
     <div className={`${direction}-arrow`} style={style} onClick={onArrowClick}>
-      { symbol }
+      {symbol}
     </div>
   );
 }
