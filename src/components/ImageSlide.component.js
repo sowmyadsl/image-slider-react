996b11ffc6dc69ce3e36aef950f33bbc32c4c87a
sowmya dinavahi <sowmya.dsl@gmail.com>
2020-01-19 22:43:00 -0800

    Refactor styling in imageSlide component

--- src/components/ImageSlide.component.js
+++ src/components/ImageSlide.component.js
@@ -1,28 +1,31 @@
-import React from 'react';
+import React from "react";
 
 function ImageSlide(props) {
-  const { image } = props;
+  const { image, onHoverOut, onHoverOver } = props;
   const style = {
     backgroundImage: `url(${image.location})`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: '50% 60%',
-    height: '100%',
-    width: '100%',
-    display: 'inline-block'
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "50% 60%",
+    height: "100%",
+    width: "100%",
+    display: "inline-block"
   };
-  const linkStyle= {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    top: '0',
-    left: '0',
-    zIndex: '1'
-  }
+  const linkStyle = {
+    display: "inline-block",
+    width: "100%",
+    height: "100%",
+    zIndex: "1"
+  };
 
   return (
-    <div className="image-slide" style={style}>
-      <a href={image.url} target="_blank">
+    <div
+      className="image-slide"
+      style={style}
+      onMouseOver={onHoverOver}
+      onMouseOut={onHoverOut}
+    >
+      <a href={image.url} target="_blank" aria-label={image.name}>
         <span className="no-content" style={linkStyle}></span>
       </a>
     </div>
